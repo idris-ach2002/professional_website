60eefc6679ff06bc4f695586324d74ad8735d12b
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-01 18:30:35 +0200

    Génération du projet Spring

---
+++ src/main/java/sorbonne/professional_website/ProfessionalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package sorbonne.professional_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sorbonne/professional_website/ProfessionalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package sorbonne.professional_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
