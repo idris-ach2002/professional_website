ad1263f507a6aed1c0b812c0a6d6d0ea22dbd26b
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-03 10:33:12 +0200

    Upadte: Project Links encapsulé

--- src/main/java/sorbonne/professional_website/entity/Project.java
+++ src/main/java/sorbonne/professional_website/entity/Project.java
@@ -2,6 +2,7 @@ package sorbonne.professional_website.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import sorbonne.professional_website.entity.enumerations.ProjectLinkType;
 import sorbonne.professional_website.entity.enumerations.ProjectStatus;
 
 import java.time.LocalDate;
@@ -101,4 +102,25 @@ public class Project {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="user_id")
     private User user;
+
+
+    @Embeddable
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class ProjectLink {
+
+        @Enumerated(EnumType.STRING)
+        @Column(name = "link_type", nullable = false, length = 50)
+        private ProjectLinkType type;
+
+        @Column(name = "label", length = 100)
+        private String label;
+
+        @Column(name = "url", nullable = false, length = 512)
+        private String url;
+    }
+
 }
