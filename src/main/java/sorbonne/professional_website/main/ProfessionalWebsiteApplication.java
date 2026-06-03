85221ce7d1a878f70780ce41626fb677a2718a1c
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-03 10:42:19 +0200

    Update: Déplacement du point d'entrée vers main

--- src/main/java/sorbonne/professional_website/main/ProfessionalWebsiteApplication.java
+++ src/main/java/sorbonne/professional_website/main/ProfessionalWebsiteApplication.java
@@ -1,4 +1,4 @@
-package sorbonne.professional_website;
+package sorbonne.professional_website.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
