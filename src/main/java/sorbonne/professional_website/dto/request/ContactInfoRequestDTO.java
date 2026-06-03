9259d4e8fe1194debed4a915db26663764632a6a
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-03 16:40:42 +0200

    Feat: Validation des données avec Spring Validation

--- src/main/java/sorbonne/professional_website/dto/request/ContactInfoRequestDTO.java
+++ src/main/java/sorbonne/professional_website/dto/request/ContactInfoRequestDTO.java
@@ -1,9 +1,14 @@
 package sorbonne.professional_website.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import sorbonne.professional_website.entity.enumerations.Contact;
 
 public record ContactInfoRequestDTO(
+        @NotNull(message = "Le type de contact est obligatoire.")
         Contact type,
+
+        @NotBlank(message = "La valeur du contact est obligatoire.")
         String value
 ) {
 }
