a6cde178c30941acb6ebcd5f665a28e64bba935f
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-03 14:34:20 +0200

    feat: Timeline controller

---
+++ src/main/java/sorbonne/professional_website/controller/TimelineController.java
@@ -0,0 +1,52 @@
+package sorbonne.professional_website.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sorbonne.professional_website.dto.request.TimelineRequestDTO;
+import sorbonne.professional_website.dto.response.TimelineResponseDTO;
+import sorbonne.professional_website.service.TimelineService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/timelines")
+public class TimelineController {
+
+    private final TimelineService srvTimeline;
+
+    public TimelineController(TimelineService srvTimeline) {
+        this.srvTimeline = srvTimeline;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTimeline(@RequestBody TimelineRequestDTO timelineRequestDTO) {
+        srvTimeline.createTimeline(timelineRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimelineResponseDTO>> getAllTimelines() {
+        return ResponseEntity.ok(srvTimeline.getAllTimelines());
+    }
+
+    @GetMapping("/{timelineId}")
+    public ResponseEntity<TimelineResponseDTO> getTimelineById(@PathVariable Long timelineId) {
+        return ResponseEntity.ok(srvTimeline.getTimelineById(timelineId));
+    }
+
+    @PutMapping("/{timelineId}")
+    public ResponseEntity<Void> updateTimeline(
+            @PathVariable Long timelineId,
+            @RequestBody TimelineRequestDTO timelineRequestDTO
+    ) {
+        srvTimeline.updateTimeline(timelineId, timelineRequestDTO);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{timelineId}")
+    public ResponseEntity<Void> deleteTimeline(@PathVariable Long timelineId) {
+        srvTimeline.deleteTimeline(timelineId);
+        return ResponseEntity.noContent().build();
+    }
+}
