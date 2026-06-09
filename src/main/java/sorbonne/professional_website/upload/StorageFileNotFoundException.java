6b93a4e2de729d2190cc7734ad135638d7a77525
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-09 15:32:34 +0200

    Feat : Exception fichier introuvable lié à l'upload

---
+++ src/main/java/sorbonne/professional_website/upload/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package sorbonne.professional_website.upload;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
