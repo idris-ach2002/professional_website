e5f7d037ce08eca297de45492d4564f68855c38d
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-09 15:32:03 +0200

    Feat : Exception générique d'upload

---
+++ src/main/java/sorbonne/professional_website/upload/StorageException.java
@@ -0,0 +1,12 @@
+package sorbonne.professional_website.upload;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
