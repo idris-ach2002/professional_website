b8f33a741c3460e8c2d62b6ef4115fd08e9a11e4
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-09 15:30:50 +0200

    Feat : Façade d'upload

--- src/main/java/sorbonne/professional_website/ProfessionalWebsiteApplication.java
+++ src/main/java/sorbonne/professional_website/ProfessionalWebsiteApplication.java
@@ -2,8 +2,11 @@ package sorbonne.professional_website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import sorbonne.professional_website.upload.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class ProfessionalWebsiteApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/sorbonne/professional_website/upload/StorageService.java
@@ -0,0 +1,23 @@
+package sorbonne.professional_website.upload;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public interface StorageService {
+
+    void init();
+
+    void store(MultipartFile file);
+
+    List<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
+
+}
