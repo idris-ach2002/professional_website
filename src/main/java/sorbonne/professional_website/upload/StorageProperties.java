da30c538ef293bef9b713001f3bc2474dea56160
Idris Achabou <idris.achabou@etu.sorbonne-universite.fr>
2026-06-09 15:31:34 +0200

    Feat : gestion du répertoire de sauvegarde des fichiers

---
+++ src/main/java/sorbonne/professional_website/upload/StorageProperties.java
@@ -0,0 +1,19 @@
+package sorbonne.professional_website.upload;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@Builder
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+}
